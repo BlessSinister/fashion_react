a39f4dc3f22e242dfae4c15dc46972b995eca943
Bless <ig.marks@yandex.ru>
2023-02-20 01:32:28 +0300

    cont/social_blog

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,6 +10,17 @@ import card9 from '../components/img/card9.png'
 import card10 from '../components/img/card10.png'
 import card11 from '../components/img/card11.png'
 import card7 from '../components/img/card7.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFacebookF,
+  faGooglePlusG,
+  faInstagram,
+  faPinterest,
+  faTwitter,
+  faVimeoV,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons'
+import { faBasketball } from '@fortawesome/free-solid-svg-icons'
 
 function Main() {
   return (
@@ -144,23 +155,6 @@ function Main() {
               sit voluptatem accusantium doloremque <br /> laudantium, totam rem
             </p>
           </div>
-          {/* <img src={card7} />
-
-          <div className="card-content">
-            <p className="main__item__title">Summer</p>
-            <p className="main__item__contetn">
-              One of Saturn’s largest rings may be newer than anyone
-            </p>
-            <p className="main__item__date">
-              June 6, 2019 By Rickie Baroch 6 comments
-            </p>
-            <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem sed ut perspiciatis
-              unde omnis iste natus error sit voluptatem accusantium doloremque
-              laudantium, totam rem
-            </p>
-          </div> */}
         </article>
         <article>
           <div className="row">
@@ -244,7 +238,7 @@ function Main() {
       >
         <div className="social__section__title">About the author</div>
         <div className="row">
-          <div className="col ">
+          <div className="col">
             <div className="card">
               <div className="card-image">
                 <img src={card10} alt="карточка" />
@@ -262,11 +256,164 @@ function Main() {
           </div>
         </div>
         <div className=" social__section__title"> Featured posts</div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
+        <div className="row">
+          <div className="col ">
+            <div className="card">
+              <div className="card-image">
+                <img src={card10} alt="карточка" />
+              </div>
+              <div className="card-content">
+                <p className="main__item__title">Summer</p>
+                <p className="main__item__contetn">
+                  One of Saturn’s largest rings <br /> may be newer than anyone
+                </p>
+                <p className="main__item__date">
+                  June 6, 2019 By Rickie Baroch
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <div className="card-image">
+                <img src={card10} alt="карточка" />
+              </div>
+              <div className="card-content">
+                <p className="main__item__title">Summer</p>
+                <p className="main__item__contetn">
+                  One of Saturn’s largest rings <br /> may be newer than anyone
+                </p>
+                <p className="main__item__date">
+                  June 6, 2019 By Rickie Baroch
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col ">
+            <div className="card">
+              <div className="card-image">
+                <img src={card10} alt="карточка" />
+              </div>
+              <div className="card-content">
+                <p className="main__item__title">Summer</p>
+                <p className="main__item__contetn">
+                  One of Saturn’s largest rings <br /> may be newer than anyone
+                </p>
+                <p className="main__item__date">
+                  June 6, 2019 By Rickie Baroch
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="social__categories_block">
+          <div className="social__section__title">Categories</div>
+          <div className="social__section">
+            <div className="social__section__item">
+              <div>Fashion</div>
+              <div>(23)</div>
+            </div>
+            <div className="social__section__item">
+              <div>Style & clothes</div>
+              <div>(7)</div>
+            </div>
+            <div className="social__section__item">
+              <div>Minimalism</div>
+              <div>(16)</div>
+            </div>
+            <div className="social__section__item">
+              <div>Black & White</div>
+              <div>(5)</div>
+            </div>
+            <div className="social__section__item last">
+              <div>Modern clothes</div>
+              <div>(12)</div>
+            </div>
+          </div>
+        </div>
+        <div className="social__section__title">Social media</div>
+        <div className="social__section__item__container">
+          <div className="social__section__item_wrapper">
+            <FontAwesomeIcon icon={faFacebookF} />
+            <div>
+              <p>
+                32k <br /> likes
+              </p>
+            </div>
+          </div>
+          <div className="social__section__item_wrapper">
+            <FontAwesomeIcon icon={faPinterest} />
+            <div>
+              <p>
+                814 <br />
+                followers
+              </p>
+            </div>
+          </div>
+          <div className="social__section__item_wrapper">
+            <FontAwesomeIcon icon={faVimeoV} />
+            <div>
+              <p>
+                165 <br />
+                followers
+              </p>
+            </div>
+          </div>
+          <div className="social__section__item_wrapper">
+            <FontAwesomeIcon icon={faBasketball} />
+            <div>
+              <p>
+                6k <br />
+                followers
+              </p>
+            </div>
+          </div>
+          <div className="social__section__item_wrapper">
+            <FontAwesomeIcon icon={faTwitter} />
+            <div>
+              <p>
+                130r <br /> followers
+              </p>
+            </div>
+          </div>
+          <div className="social__section__item_wrapper">
+            <FontAwesomeIcon icon={faPinterest} />
+            <div>
+              <p>
+                37k <br /> followers
+              </p>
+            </div>
+          </div>
+          <div className="social__section__item_wrapper">
+            <FontAwesomeIcon icon={faInstagram} />
+            <div>
+              <p>
+                854k <br /> followers
+              </p>
+            </div>
+          </div>
+          <div className="social__section__item_wrapper">
+            <FontAwesomeIcon icon={faYoutube} />
+            <div>
+              <p>
+                52k <br /> subscribers
+              </p>
+            </div>
+          </div>
+          <div className="social__section__item_wrapper">
+            <FontAwesomeIcon icon={faGooglePlusG} />
+            <div>
+              <p>
+                642 <br />
+                followers
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   )
